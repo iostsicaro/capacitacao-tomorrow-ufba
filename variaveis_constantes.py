75c8ee9a4eee8f0a50b9803e47d532e38a4dbbf7
Icaro <iostsicaro@gmail.com>
2024-06-05 14:04:52 -0300

    Criando e testando variáveis.

---
+++ variaveis_constantes.py
@@ -0,0 +1,25 @@
+#CRIANDO VARIÁVEIS
+nome = "Guilherme"
+idade = 28
+
+print(nome, idade)
+
+nome = "Giovana"
+
+print(nome)
+
+#É POSSÍVEL CRIAR VÁRIAS VARIÁVEIS EM APENAS UMA LINHA
+nome, idade = ("Icaro", 27)
+
+print(nome, idade)
+
+#UTILIZANDO DO SNAKE CASE PARA BOAS PRÁTICAS NAS CRIAÇÕES DE VARIÁVEIS
+limite_saque_diario = 1000
+limiteSaqueDiario = 1000
+
+print(limite_saque_diario, limiteSaqueDiario)
+
+#UTILIZANDO A CONVENÇÃO DO PYTHON PARA CRIAR VARIÁVEIS CONSTANTES
+BRAZILIAN_STATES = ["sp", "ba", "rj"]
+
+print(BRAZILIAN_STATES)
