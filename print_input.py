675038233d291eddd4770b0fa8ee00cfcb1d3cff
Icaro <iostsicaro@gmail.com>
2024-06-08 02:32:33 -0300

    Testando entrada e saída em python.

---
+++ print_input.py
@@ -0,0 +1,7 @@
+#TESTANDO ENTRADAS E SAÍDAS
+nome = input("Informe o seu nome: ")
+sobrenome = input("Informe seu sobrenome: ")
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n")
+print(nome, sobrenome, sep=" #")
