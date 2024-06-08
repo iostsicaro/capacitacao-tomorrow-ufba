f1bcc752bbe276b043cc90b18a0c5ffaddd0226b
Icaro <iostsicaro@gmail.com>
2024-06-08 02:32:13 -0300

    Convertendo tipos de dados em python.

---
+++ convertendo_tipos.py
@@ -0,0 +1,14 @@
+#TESTANDO CONVERSÕES
+print(int(1.9562893729))
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+#A DIVISÃO COM // NOS RETORNA O RESULTADO DA DIVISÃO PORÉM COM A PARTE INTEIRA SEM PONTO FLUTUANTE
+print(100 / 2)
+print(100 // 2)
