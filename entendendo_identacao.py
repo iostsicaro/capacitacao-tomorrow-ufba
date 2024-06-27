433e78f665c9b26c430619c5982e1a57073c43ec
Icaro <iostsicaro@gmail.com>
2024-06-26 22:05:39 -0300

    Entendendo identação da linguagem.

---
+++ entendendo_identacao.py
@@ -0,0 +1,11 @@
+def sacar(valor):
+    saldo = 500
+    
+    if saldo >= valor:
+        print("Valor sacado.")
+        print("Retire o seu dinheiro na boca do caixa.")
+    
+    print("Obrigado por ser nosso cliente, tenha um bom dia!!")
+
+
+sacar(400)
