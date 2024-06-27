e161a9a358c386779abfaa67bc8478796f00a22e
Icaro <iostsicaro@gmail.com>
2024-06-26 22:05:23 -0300

    Operadores de identidade.

---
+++ operadores_identidade.py
@@ -0,0 +1,8 @@
+saque = 100
+limite = 100
+
+# VERIFICA SE OS DOIS OBJETOS OCUPAM A MESMA POSIÇÃO DE ESPAÇO NA MEMÓRIA
+print(saque is limite)
+
+# VERIFICA SE OS DOIS OBJETOS NÃO OCUPAM A MESMA POSIÇÃO DE ESPCAÇO NA MEMÓRIA
+print(limite is not saque)
